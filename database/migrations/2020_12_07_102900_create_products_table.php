5abfbdbbc7caec237dfea567e541535ba650b245
giannurwana19 <giannurwana19@gmail.com>
2020-12-07 11:04:47 +0700

    #6 pembuatan table database dengan migration

---
+++ database/migrations/2020_12_07_102900_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('sku');
+            $table->string('name');
+            $table->string('slug');
+            $table->decimal('price', 15, 2);
+            $table->decimal('weight', 10, 2);
+            $table->decimal('width', 10, 2);
+            $table->decimal('height', 10, 2);
+            $table->decimal('depth', 10, 2);
+            $table->text('short_description');
+            $table->text('description');
+            $table->integer('status');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
