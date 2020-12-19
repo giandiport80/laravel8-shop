4e65ea7a0b40566b5be6696b935db03f21b01d2b
giannurwana19 <giannurwana19@gmail.com>
2020-12-19 16:34:34 +0700

    #18 kirim email detail order dengan markdown & queue

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderRequest;
+use App\Jobs\SendMailOrderReceived;
+use App\Mail\OrderShipped;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\ProductInventory;
@@ -13,8 +15,10 @@ use Darryldecode\Cart\CartCondition;
 use Darryldecode\Cart\Facades\CartFacade;
 use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Queue\Jobs\Job;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class OrderController extends Controller
 {
@@ -115,125 +119,13 @@ class OrderController extends Controller
         $params = $request->except('_token');
 
         $order = DB::transaction(function () use ($params) {
-            $destination = isset($params['ship_to']) ? $params['shipping_city_id'] : $params['city_id'];
-            $selectedShipping = $this->getSelectedShipping($destination, $this->getTotalWeight(), $params['shipping_service']);
+            $order = $this->_saveOrder($params);
 
-            $baseTotalPrice = CartFacade::getSubTotal();
-            $taxAmount = CartFacade::getCondition('TAX 10%')->getCalculatedValue(CartFacade::getSubTotal());
-            $taxPercent = (float) CartFacade::getCondition('TAX 10%')->getValue();
-            $shippingCost = $selectedShipping['cost'];
-            $discountAmount = 0;
-            $discountPercent = 0;
-            $grandTotal = ($baseTotalPrice + $taxAmount + $shippingCost) - $discountAmount;
-
-            $orderDate = date('Y-m-d H:i:s');
-            $paymentDue = (new DateTime($orderDate))->modify('+7 day')->format('Y-m-d H:i:s');
-
-            $orderParams = [
-                'user_id' => Auth::id(),
-                'code' => Order::generateCode(),
-                'status' => Order::CREATED,
-                'order_date' => $orderDate,
-                'payment_due' => $paymentDue,
-                'payment_status' => Order::UNPAID,
-                'base_total_price' => $baseTotalPrice,
-                'tax_amount' => $taxAmount,
-                'tax_percent' => $taxPercent,
-                'discount_amount' => $discountAmount,
-                'discount_percent' => $discountPercent,
-                'shipping_cost' => $shippingCost,
-                'grand_total' => $grandTotal,
-                'node' => $params['note'],
-                'customer_first_name' => $params['first_name'],
-                'customer_last_name' => $params['last_name'],
-                'customer_company' => $params['company'],
-                'customer_address1' => $params['address1'],
-                'customer_address2' => $params['address2'],
-                'customer_phone' => $params['phone'],
-                'customer_email' => $params['email'],
-                'customer_city_id' => $params['city_id'],
-                'customer_province_id' => $params['province_id'],
-                'customer_postcode' => $params['postcode'],
-                'shipping_courier' => $selectedShipping['courier'],
-                'shipping_service_name' => $selectedShipping['service'],
-            ];
-
-            $order = Order::create($orderParams);
             // dd($order->toArray()); // .. 4
-            $cartItems = CartFacade::getContent();
 
-            if($order && $cartItems){
-                foreach($cartItems as $item){
-                    $itemTaxAmount = 0;
-                    $itemTaxPercent = 0;
-                    $itemDiscountAmount = 0;
-                    $itemDiscountPercent = 0;
-                    $itemBaseTotal = $item->quantity * $item->price;
-                    $itemSubTotal = $itemBaseTotal + $itemTaxAmount - $itemDiscountAmount;
+            $this->_saveOrderItems($order);
 
-                    $product = isset($item->associatedModel->parent) ? $item->associatedModel->parent : $item->associatedModel;
-
-                    $orderItemParams = [
-                        'order_id' => $order->id,
-                        'product_id' => $item->associatedModel->id,
-                        'qty' => $item->quantity,
-                        'base_price' => $item->price,
-                        'base_total' => $itemBaseTotal,
-                        'tax_amount' => $itemTaxAmount,
-                        'tax_percent' => $itemTaxPercent,
-                        'discount_amount' => $itemDiscountAmount,
-                        'discount_percent' => $itemDiscountPercent,
-                        'sub_total' => $itemSubTotal,
-                        'sku' => $item->associatedModel->sku,
-                        'type' => $product->type,
-                        'name' => $item->name,
-                        'weight' => $item->associatedModel->weight,
-                        'attributes' => json_encode($item->attributes),
-                    ];
-
-                    // dd($orderItemParams);
-
-                    $orderItem = OrderItem::create($orderItemParams);
-
-                    // dd($orderItem->toArray());
-
-                    if ($orderItem) {
-                        ProductInventory::reduceStock($orderItem->product_id, $orderItem->qty);
-                    }
-                }
-            }
-
-            $shippingFirstName = isset($params['ship_to']) ? $params['shipping_first_name'] : $params['first_name'];
-            $shippingLastName = isset($params['ship_to']) ? $params['shipping_last_name'] : $params['last_name'];
-            $shippingCompany = isset($params['ship_to']) ? $params['shipping_company'] : $params['company'];
-            $shippingAddress1 = isset($params['ship_to']) ? $params['shipping_address1'] : $params['address1'];
-            $shippingAddress2 = isset($params['ship_to']) ? $params['shipping_address2'] : $params['address2'];
-            $shippingPhone = isset($params['ship_to']) ? $params['shipping_phone'] : $params['phone'];
-            $shippingEmail = isset($params['ship_to']) ? $params['shipping_email'] : $params['email'];
-            $shippingCityId = isset($params['ship_to']) ? $params['shipping_city_id'] : $params['city_id'];
-            $shippingProvinceId = isset($params['ship_to']) ? $params['shipping_province_id'] : $params['province_id'];
-            $shippingPostcode = isset($params['ship_to']) ? $params['shipping_postcode'] : $params['postcode'];
-
-            $shipmentParams = [
-                'user_id' => Auth::user()->id,
-                'order_id' => $order->id,
-                'status' => Shipment::PENDING,
-                'total_qty' => CartFacade::getTotalQuantity(),
-                'total_weight' => $this->getTotalWeight(),
-                'first_name' => $shippingFirstName,
-                'last_name' => $shippingLastName,
-                'address1' => $shippingAddress1,
-                'address2' => $shippingAddress2,
-                'phone' => $shippingPhone,
-                'email' => $shippingEmail,
-                'city_id' => $shippingCityId,
-                'province_id' => $shippingProvinceId,
-                'postcode' => $shippingPostcode,
-            ];
-
-            // dd($shipmentParams); // .. 4
-
-            Shipment::create($shipmentParams);
+            $this->_saveShipment($order, $params);
 
             return $order;
 
@@ -241,6 +133,8 @@ class OrderController extends Controller
 
         if($order){
             CartFacade::clear();
+
+            $this->_sendEmailOrderReceived($order); // .. 8
 
             session()->flash('success', 'Thank you, Your order has been received!');
 
@@ -250,10 +144,140 @@ class OrderController extends Controller
         return redirect('orders/checkout');
     }
 
+    private function _saveOrder($params)
+    {
+        $destination = isset($params['ship_to']) ? $params['shipping_city_id'] : $params['city_id'];
+        $selectedShipping = $this->getSelectedShipping($destination, $this->getTotalWeight(), $params['shipping_service']);
+
+        $baseTotalPrice = CartFacade::getSubTotal();
+        $taxAmount = CartFacade::getCondition('TAX 10%')->getCalculatedValue(CartFacade::getSubTotal());
+        $taxPercent = (float) CartFacade::getCondition('TAX 10%')->getValue();
+        $shippingCost = $selectedShipping['cost'];
+        $discountAmount = 0;
+        $discountPercent = 0;
+        $grandTotal = ($baseTotalPrice + $taxAmount + $shippingCost) - $discountAmount;
+
+        $orderDate = date('Y-m-d H:i:s');
+        $paymentDue = (new DateTime($orderDate))->modify('+7 day')->format('Y-m-d H:i:s');
+
+        $orderParams = [
+            'user_id' => Auth::id(),
+            'code' => Order::generateCode(),
+            'status' => Order::CREATED,
+            'order_date' => $orderDate,
+            'payment_due' => $paymentDue,
+            'payment_status' => Order::UNPAID,
+            'base_total_price' => $baseTotalPrice,
+            'tax_amount' => $taxAmount,
+            'tax_percent' => $taxPercent,
+            'discount_amount' => $discountAmount,
+            'discount_percent' => $discountPercent,
+            'shipping_cost' => $shippingCost,
+            'grand_total' => $grandTotal,
+            'note' => $params['note'],
+            'customer_first_name' => $params['first_name'],
+            'customer_last_name' => $params['last_name'],
+            'customer_company' => $params['company'],
+            'customer_address1' => $params['address1'],
+            'customer_address2' => $params['address2'],
+            'customer_phone' => $params['phone'],
+            'customer_email' => $params['email'],
+            'customer_city_id' => $params['city_id'],
+            'customer_province_id' => $params['province_id'],
+            'customer_postcode' => $params['postcode'],
+            'shipping_courier' => $selectedShipping['courier'],
+            'shipping_service_name' => $selectedShipping['service'],
+        ];
+
+        return Order::create($orderParams);
+    }
+
+    private function _saveOrderItems($order)
+    {
+        $cartItems = CartFacade::getContent();
+
+        if ($order && $cartItems) {
+            foreach ($cartItems as $item) {
+                $itemTaxAmount = 0;
+                $itemTaxPercent = 0;
+                $itemDiscountAmount = 0;
+                $itemDiscountPercent = 0;
+                $itemBaseTotal = $item->quantity * $item->price;
+                $itemSubTotal = $itemBaseTotal + $itemTaxAmount - $itemDiscountAmount;
+
+                $product = isset($item->associatedModel->parent) ? $item->associatedModel->parent : $item->associatedModel;
+
+                $orderItemParams = [
+                    'order_id' => $order->id,
+                    'product_id' => $item->associatedModel->id,
+                    'qty' => $item->quantity,
+                    'base_price' => $item->price,
+                    'base_total' => $itemBaseTotal,
+                    'tax_amount' => $itemTaxAmount,
+                    'tax_percent' => $itemTaxPercent,
+                    'discount_amount' => $itemDiscountAmount,
+                    'discount_percent' => $itemDiscountPercent,
+                    'sub_total' => $itemSubTotal,
+                    'sku' => $item->associatedModel->sku,
+                    'type' => $product->type,
+                    'name' => $item->name,
+                    'weight' => $item->associatedModel->weight,
+                    'attributes' => json_encode($item->attributes),
+                ];
+
+                // dd($orderItemParams);
+
+                $orderItem = OrderItem::create($orderItemParams);
+
+                // dd($orderItem->toArray());
+
+                if ($orderItem) {
+                    ProductInventory::reduceStock($orderItem->product_id, $orderItem->qty);
+                }
+            }
+        }
+    }
+
+    private function _saveShipment($order, $params)
+    {
+        $shippingFirstName = isset($params['ship_to']) ? $params['shipping_first_name'] : $params['first_name'];
+        $shippingLastName = isset($params['ship_to']) ? $params['shipping_last_name'] : $params['last_name'];
+        $shippingCompany = isset($params['ship_to']) ? $params['shipping_company'] : $params['company'];
+        $shippingAddress1 = isset($params['ship_to']) ? $params['shipping_address1'] : $params['address1'];
+        $shippingAddress2 = isset($params['ship_to']) ? $params['shipping_address2'] : $params['address2'];
+        $shippingPhone = isset($params['ship_to']) ? $params['shipping_phone'] : $params['phone'];
+        $shippingEmail = isset($params['ship_to']) ? $params['shipping_email'] : $params['email'];
+        $shippingCityId = isset($params['ship_to']) ? $params['shipping_city_id'] : $params['city_id'];
+        $shippingProvinceId = isset($params['ship_to']) ? $params['shipping_province_id'] : $params['province_id'];
+        $shippingPostcode = isset($params['ship_to']) ? $params['shipping_postcode'] : $params['postcode'];
+
+        $shipmentParams = [
+            'user_id' => Auth::user()->id,
+            'order_id' => $order->id,
+            'status' => Shipment::PENDING,
+            'total_qty' => CartFacade::getTotalQuantity(),
+            'total_weight' => $this->getTotalWeight(),
+            'first_name' => $shippingFirstName,
+            'last_name' => $shippingLastName,
+            'address1' => $shippingAddress1,
+            'address2' => $shippingAddress2,
+            'phone' => $shippingPhone,
+            'email' => $shippingEmail,
+            'city_id' => $shippingCityId,
+            'province_id' => $shippingProvinceId,
+            'postcode' => $shippingPostcode,
+        ];
+
+        // dd($shipmentParams); // .. 4
+
+        return Shipment::create($shipmentParams);
+    }
+
     public function received($orderID)
     {
         $this->data['order'] = Order::where('id', $orderID)->where('user_id', Auth::id())->firstOrFail();
 
+        // $this->_sendEmailOrderReceived($this->data['order']); // .. 6
         return $this->load_theme('orders.received', $this->data);
     }
 
@@ -261,6 +285,15 @@ class OrderController extends Controller
 
     // ! private method
     // ! ============================================================================
+
+    private function _sendEmailOrderReceived($order)
+    {
+        // $message = new OrderShipped($order); // .. 5
+        // Mail::to(Auth::user()->email)->send($message);
+
+        SendMailOrderReceived::dispatch($order, Auth::user()); // .. 7
+    }
+
 
     /**
      * Get selected shipping from user input
@@ -412,3 +445,39 @@ class OrderController extends Controller
 
 // k: order pertama diterima
 // http://127.0.0.1:8000/orders/received/8
+
+// p: clue 5
+// kita kirim email berdasarkan class email OrderShipped yg kita buat
+
+// * membuat queue
+// ganti QUEUE_CONNECTION=database pada .env
+// > php artisan queue:table
+
+// > php artisan migrate
+
+// * membuat job class
+// > php artisan make:job SendMailOrderReceived
+
+// p: clue 6
+// setelah kita menggunakan job class
+// kita tidak mengirimkan lewat method received
+// tapi pada job method handle pada class SendMailOrderReceived yg kita buat
+
+// p: clue 7
+// kita jalankan job class nya
+// kemudian akan create data queue di tb jobs nya
+// untuk mengeksekusi queue nya kita gunakan
+// > php artisan queue:listen
+// pada cmd akan seperti ini
+// [2020-12-19 15:51:12][1] Processing: App\Jobs\SendMailOrderReceived
+// [2020-12-19 15:51:15][1] Processed:  App\Jobs\SendMailOrderReceived
+
+// ketika job queue sukses, akan di hapus dari record
+// dan email nya dikirim
+
+// kita bisa tambahkan delay 1 menit
+// SendMailOrderReceived::dispatch($order, Auth::user())->delay(now()->addMinutes(1));
+
+// p: clue 8
+// kita tempatkan di tempat yang pas
+// setelah keranjang dihapus, kita kirim emailnya
---
+++ app/Jobs/SendMailOrderReceived.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\OrderShipped;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendMailOrderReceived implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($order, $user)
+    {
+        $this->order = $order;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $orderReceivedEmail = new OrderShipped($this->order);
+
+        // kirim email 
+        Mail::to($this->user->email)->send($orderReceivedEmail);
+    }
+}
